47c813de27a7eaa18c2173f30c37e836b4fb6dbb
Fernando Pinto <fschultepinto@gmail.com>
2023-08-29 16:56:51 -0300

    🚧 set dashboard menu component

--- src/components/DashMenu/index.tsx
+++ src/components/DashMenu/index.tsx
@@ -1,3 +1,25 @@
-export function Menu() {
-  return <></>;
+import { Box, HStack, Text } from "@chakra-ui/react";
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+interface MenuProps {
+  title: string;
+  icon: string;
+}
+
+export function Menu(props: MenuProps) {
+  return (
+    <Box
+      color="purple.900"
+      display="flex"
+      flexDir="row"
+      w="full"
+      borderY="1px solid rgba(79, 79, 79, .08)"
+    >
+      <Box h="full" w="4px" bgColor="purple.900" />
+      <HStack py="40px" pl="16px" gap="10px">
+        <Icon width="24px" height="24px" icon={props.icon} />
+        <Text fontSize="14px">{props.title}</Text>
+      </HStack>
+    </Box>
+  );
 }
