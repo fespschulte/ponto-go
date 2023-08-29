3fe17f8ff7fc07fc28ca2df03c248059207d2674
Fernando Pinto <fschultepinto@gmail.com>
2023-08-28 22:39:22 -0300

    🚧set header structure and styling

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export function LandingPage() {
   return (
@@ -8,6 +9,27 @@ export function LandingPage() {
       backgroundImage={"assets/bg-landing.png"}
       bgRepeat={"no-repeat"}
       backgroundSize={"cover"}
-    ></Box>
+    >
+      <Flex flexDir="column" px="120px">
+        <Flex align="center" justify="space-between" mt="45px">
+          <Image w="164px" src="/assets/pontogo-logo.svg" alt="Logo PontoGo" />
+          <Flex align="center" gap="40px">
+            <Flex gap="50px">
+              <Link to="/">
+                <Text color="white">Início</Text>
+              </Link>
+              <Link to="/">
+                <Text color="white">Planos</Text>
+              </Link>
+            </Flex>
+            <Link to="/login">
+              <Button size="sm" variant="solidWhite">
+                Fazer Login
+              </Button>
+            </Link>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
