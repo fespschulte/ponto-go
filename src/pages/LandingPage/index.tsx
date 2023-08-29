d1562c67acea8c1cdaadaa3b3cb300581660ed19
Fernando Pinto <fschultepinto@gmail.com>
2023-08-28 22:29:04 -0300

    🚧add background image

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,3 +1,13 @@
+import { Box } from "@chakra-ui/react";
+
 export function LandingPage() {
-  return <></>;
+  return (
+    <Box
+      w="100vw"
+      h="100vh"
+      backgroundImage={"assets/bg-landing.png"}
+      bgRepeat={"no-repeat"}
+      backgroundSize={"cover"}
+    ></Box>
+  );
 }
