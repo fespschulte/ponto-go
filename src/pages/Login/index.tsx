d70eadc54940e50cc5d13bce16586bf328d2d1d9
Fernando Pinto <fschultepinto@gmail.com>
2023-08-29 17:01:48 -0300

    🚧set page structure and styling

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,54 @@
+import {
+  HStack,
+  Image,
+  Heading,
+  Text,
+  VStack,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
+import { Link } from "react-router-dom";
+
 export function Login() {
-  return <></>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <HStack gap="150px">
+        <VStack maxW="585" textAlign="center" align="center" color="purple.900">
+          <Image mb="20px" src="/assets/image-login.png"></Image>
+          <Heading mb="10px" fontSize="40px">
+            Bem vindo ao PontoGo
+          </Heading>
+          <Text fontSize="25px" opacity={0.7}>
+            Aqui você fará toda gestão do <br /> seu sistema de pontos.
+          </Text>
+        </VStack>
+        <VStack align="flex-start" gap="30px">
+          <Image src="/assets/pontogo-logo-purple.svg"></Image>
+          <VStack gap="0px" align="flex-start" w="full">
+            <Heading color="purple.900" fontSize="40px" mb="23px">
+              Faça login
+            </Heading>
+            <Input
+              type="email"
+              name="email"
+              label="Email"
+              placeholder="exemplo@email.com"
+            />
+            <Input
+              type="password"
+              name="password"
+              label="Senha"
+              placeholder="***********"
+              icon="ant-design:eye-filled"
+            />
+            <Link to="/">Esqueci minha senha</Link>
+          </VStack>
+          <Button size="lg" variant="darkPurpleWhite">
+            Entrar
+          </Button>
+        </VStack>
+      </HStack>
+    </Flex>
+  );
 }
