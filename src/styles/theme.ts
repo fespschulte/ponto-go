8e27d569ec25704966bb835a9a3d0e71ebcec637
Fernando Pinto <fschultepinto@gmail.com>
2023-08-28 22:10:15 -0300

    💄set all font weights on global theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,10 @@
 import { defineStyleConfig, extendTheme } from "@chakra-ui/react";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/800.css";
 
 const Button = defineStyleConfig({
   baseStyle: {
