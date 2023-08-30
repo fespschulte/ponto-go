62751c2d0080d1a0c0099a7fee5db2032998a035
Fernando Pinto <fschultepinto@gmail.com>
2023-08-30 11:45:55 -0300

    🚧set authentication context for pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
 import { MainRoutes } from "./routes";
 import { SidebarDrawerProvider } from "./contexts/SidebarDrawerContext";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <MainRoutes />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <MainRoutes />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </AuthProvider>
   );
 }
 
