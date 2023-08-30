7c4fdd2071d6ff2cc608cc0b2fef7ba18b2f6fde
Fernando Pinto <fschultepinto@gmail.com>
2023-08-30 02:53:47 -0300

    🚧set structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/theme";
 import { MainRoutes } from "./routes";
+import { SidebarDrawerProvider } from "./contexts/SidebarDrawerContext";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <MainRoutes />
+      <SidebarDrawerProvider>
+        <MainRoutes />
+      </SidebarDrawerProvider>
     </ChakraProvider>
   );
 }
