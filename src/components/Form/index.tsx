13fa264c39f4e84dfe75c8752e20f5e46b5b6a84
Fernando Pinto <fschultepinto@gmail.com>
2023-08-31 13:34:35 -0300

    🔥remove code

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,5 +1,4 @@
 import {
-  FormControl,
   FormLabel,
   Input as ChakraInput,
   InputProps as ChakraInputProps,
@@ -17,33 +16,31 @@ interface InputProps extends ChakraInputProps {
 export function Input({ name, label, icon, ...rest }: InputProps) {
   return (
     <>
-      <FormControl>
-        {!!label && (
-          <FormLabel htmlFor={name} fontSize="xl">
-            {label}
-          </FormLabel>
+      {!!label && (
+        <FormLabel htmlFor={name} fontSize="xl">
+          {label}
+        </FormLabel>
+      )}
+      <InputGroup flexDir="column">
+        <ChakraInput
+          id={name}
+          px="20px"
+          h="45px"
+          mb="20px"
+          focusBorderColor="purple.900"
+          {...rest}
+        />
+        {!!icon && (
+          <InputRightElement
+            mt="3px"
+            alignItems="center"
+            pr="20px"
+            color="purple.900"
+          >
+            <Icon width="24px" height="24px" icon={icon} />
+          </InputRightElement>
         )}
-        <InputGroup flexDir="column">
-          <ChakraInput
-            id={name}
-            px="20px"
-            h="45px"
-            mb="20px"
-            focusBorderColor="purple.900"
-            {...rest}
-          />
-          {!!icon && (
-            <InputRightElement
-              mt="3px"
-              alignItems="center"
-              pr="20px"
-              color="purple.900"
-            >
-              <Icon width="24px" height="24px" icon={icon} />
-            </InputRightElement>
-          )}
-        </InputGroup>
-      </FormControl>
+      </InputGroup>
     </>
   );
 }
