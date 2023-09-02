d74d4a4dbb3c6b2b43a24cce22b2cd1fd6282411
Fernando Pinto <fschultepinto@gmail.com>
2023-09-02 00:17:28 -0300

    🚧set api integration

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,22 +1,59 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { useAuthToken } from "./auth";
+import {
+  ApolloClient,
+  ApolloLink,
+  concat,
+  createHttpLink,
+  gql,
+  InMemoryCache,
+} from "@apollo/client";
+import { getAccessToken } from "./auth";
 
 const httpLink = createHttpLink({
   uri: "http://test.frontend.api.brainny.cc/graphql",
 });
 
-const authLink = setContext((_, { headers }) => {
-  const token = useAuthToken();
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
+const authLink = new ApolloLink((operation, forward) => {
+  const accessToken = getAccessToken();
+  if (accessToken) {
+    operation.setContext({
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+  }
+  return forward(operation);
 });
 
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: concat(authLink, httpLink),
   cache: new InMemoryCache(),
 });
+
+export const loginMutation = gql`
+  mutation login($email: String!, $password: String!) {
+    login(input: { identifier: $email, password: $password }) {
+      jwt
+    }
+  }
+`;
+
+// import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
+// import { useAuthToken } from "./auth";
+
+// const httpLink = createHttpLink({
+//   uri: "http://test.frontend.api.brainny.cc/graphql",
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   const token = useAuthToken();
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
+
+// export const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,22 +1,68 @@
-import { useApolloClient } from "@apollo/react-hooks";
-import { useCookies } from "react-cookie";
+// import jwtDecode from "jwt-decode";
 
-const AUTH_TOKEN = "auth-token";
+// const API_URL = "http://test.frontend.api.brainny.cc/graphql";
 
-export const useAuthToken = () => {
-  const [cookies, setCookie, removeCookie] = useCookies([AUTH_TOKEN]);
-  const setAuthToken = (authToken: string) => setCookie(AUTH_TOKEN, authToken);
-  const removeAuthToken = () => removeCookie(AUTH_TOKEN);
-  return [cookies[AUTH_TOKEN], setAuthToken, removeAuthToken];
-};
+const ACCESS_TOKEN_KEY = "accessToken";
 
-export const useLogout = () => {
-  const [, , removeAuthToken] = useAuthToken();
-  const apolloClient = useApolloClient();
+export function getAccessToken() {
+  return localStorage.getItem(ACCESS_TOKEN_KEY);
+}
 
-  const logout = async () => {
-    await apolloClient.clearStore();
-    removeAuthToken();
-  };
-  return logout;
-};
+// export async function login(email: string, password: string) {
+//   const response = await fetch(`${API_URL}/login`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ email, password }),
+//   });
+//   if (!response.ok) {
+//     return null;
+//   }
+//   const { token } = await response.json();
+//   localStorage.setItem(ACCESS_TOKEN_KEY, token);
+//   return getUserFromToken(token);
+// }
+
+// export function getUser() {
+//   const token = getAccessToken();
+//   if (!token) {
+//     return null;
+//   }
+//   return getUserFromToken(token);
+// }
+
+// export function logout() {
+//   localStorage.removeItem(ACCESS_TOKEN_KEY);
+// }
+
+// function getUserFromToken(token: string) {
+//   const claims = jwtDecode(token);
+//   return {
+//     id: claims.sub,
+//     email: claims.email,
+//   };
+// }
+
+// import { useApolloClient } from "@apollo/react-hooks";
+// import { useCookies } from "react-cookie";
+
+// const AUTH_TOKEN = "auth-token";
+
+// export const useAuthToken = () => {
+//   const [cookies, setCookie, removeCookie] = useCookies([AUTH_TOKEN]);
+//   const setAuthToken = (authToken: string) => setCookie(AUTH_TOKEN, authToken);
+//   const removeAuthToken = () => removeCookie(AUTH_TOKEN);
+//   return [cookies[AUTH_TOKEN], setAuthToken, removeAuthToken];
+// };
+
+// export const useLogout = () => {
+//   const [, , removeAuthToken] = useAuthToken();
+//   const apolloClient = useApolloClient();
+
+//   const logout = async () => {
+//     await apolloClient.clearStore();
+//     removeAuthToken();
+//   };
+//   return logout;
+// };
