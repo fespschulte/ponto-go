a73d579ba8b26224f3746eebdc8f638d7e8b9ea7
Fernando Pinto <fschultepinto@gmail.com>
2023-09-02 00:20:01 -0300

    🔥remove code

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,179 +6,115 @@ import {
   VStack,
   Button,
   Flex,
-  Box,
 } from "@chakra-ui/react";
 import { Input } from "../../components/Form";
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router-dom";
-import { useMutation } from "@apollo/client";
-import { useAuthToken } from "../../services/auth";
-import { LOGIN_REQUEST } from "../../graphQL/mutations";
+import { Link, Navigate } from "react-router-dom";
+import { useState } from "react";
+// import { login } from "../../services/auth";
+// import { useMutation } from "@apollo/client";
+// import { loginMutation } from "../../services/api";
+import { useLogin } from "../../graphQL/hooks";
 
-interface LoginFormValues {
-  email: string;
-  password: string;
-}
+export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
 
-interface LoginProps {
-  loading: boolean;
-}
+  //   const [mutate] = useMutation(loginMutation);
+  const { login, loading } = useLogin();
 
-export function Login({ loading }: LoginProps) {
-  const navigate = useNavigate();
-  const [, setAuthToken] = useAuthToken(); // Use useAuthToken para acessar o token JWT
-
-  const [loginMutation] = useMutation(LOGIN_REQUEST);
-
-  const { handleSubmit, control } = useForm<LoginFormValues>();
-
-  const onSubmit: SubmitHandler<LoginFormValues> = async (values) => {
-    try {
-      const { data } = await loginMutation({
-        variables: {
-          email: values.email,
-          password: values.password,
-        },
-      });
-      if (data && data.login && data.login.user) {
-        setAuthToken(data.login.jwt);
-        // if (data.login.user.role === "admin") {
-        navigate("/dashboard");
-        // }
-        // if (data.login.user.role === "user") {
-        //   navigate("/meus-registros");
-        // }
-      }
-    } catch (error) {
-      console.error("Erro durante o login:", error);
-    }
+  const handleSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    setError(false);
+    // const user = await login(email, password);
+    const dataLogin = await login(email, password);
+    console.log("dados:", dataLogin);
+    <Navigate to="/dashboard" />;
+    console.log(error);
   };
 
-  const disableForm = loading;
-
   return (
-    <Box
-      backgroundImage={"assets/bg-login.png"}
-      bgRepeat={"no-repeat"}
-      backgroundSize={"cover"}
-    >
-      <Flex h="100vh" align="center">
-        <HStack m="auto" w="85%" justify="center" align="center" gap="150px">
-          <VStack
-            display={{ base: "none", md: "flex" }}
-            maxW={{ base: "400px", xl: "585px" }}
-            textAlign="center"
-            align="center"
+    <Flex h="100vh" align="center">
+      <HStack m="auto" w="85%" justify="center" align="center" gap="150px">
+        <VStack
+          display={{ base: "none", md: "flex" }}
+          maxW={{ base: "400px", xl: "585px" }}
+          textAlign="center"
+          align="center"
+          color="purple.900"
+        >
+          <Image mb="20px" src="/assets/image-login.png"></Image>
+          <Heading mb="10px" fontSize="40px">
+            Bem vindo ao PontoGo
+          </Heading>
+          <Text fontSize="25px" opacity={0.7}>
+            Aqui você fará toda gestão do <br /> seu sistema de pontos.
+          </Text>
+        </VStack>
+        <VStack gap="0px" w={{ base: "300px", xl: "400px" }} align="flex-start">
+          <Image src="/assets/pontogo-logo-purple.svg"></Image>
+          <Heading
+            mt="30px"
             color="purple.900"
+            fontSize={{ base: "2xl", md: "3xl", xl: "40px" }}
           >
-            <Image mb="20px" src="/assets/image-login.png"></Image>
-            <Heading mb="10px" fontSize="40px">
-              Bem vindo ao PontoGo
-            </Heading>
-            <Text fontSize="25px" opacity={0.7}>
-              Aqui você fará toda gestão do <br /> seu sistema de pontos.
-            </Text>
-          </VStack>
+            Faça login
+          </Heading>
           <VStack
+            as="form"
+            onSubmit={handleSubmit}
+            w="full"
             gap="0px"
-            w={{ base: "300px", xl: "400px" }}
             align="flex-start"
           >
-            <Image src="/assets/pontogo-logo-purple.svg"></Image>
-            <Heading
-              mt="30px"
-              color="purple.900"
-              fontSize={{ base: "2xl", md: "3xl", xl: "40px" }}
-            >
-              Faça login
-            </Heading>
-            <VStack
-              as="form"
-              onSubmit={handleSubmit(onSubmit)}
-              w="full"
-              gap="0px"
-              align="flex-start"
-            >
-              <Controller
-                name="email"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <Input
-                    type="email"
-                    label="Email"
-                    placeholder="exemplo@email.com"
-                    {...field}
-                  />
-                )}
-              />
-              <Controller
-                name="password"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <Input
-                    type="password"
-                    label="Senha"
-                    placeholder="***********"
-                    icon="ant-design:eye-filled"
-                    {...field}
-                  />
-                )}
-              />
-              <Link to="/">
-                <Text
-                  textDecorationLine="underline"
-                  mt="10px"
-                  color="purple.900"
-                  fontSize="15px"
-                >
-                  Esqueci minha senha
-                </Text>
-              </Link>
-              <Button
-                mt="30px"
-                type="submit"
-                w={{ base: "full", md: "300px", xl: "400px" }}
-                variant="darkPurpleWhite"
-                isLoading={loading}
-                disabled={disableForm}
+            <Input
+              name="email"
+              type="email"
+              label="Email"
+              placeholder="exemplo@email.com"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <Input
+              name="password"
+              type="password"
+              label="Senha"
+              placeholder="***********"
+              icon="ant-design:eye-filled"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <Link to="/">
+              <Text
+                textDecorationLine="underline"
+                mt="10px"
+                color="purple.900"
+                fontSize="15px"
               >
-                Entrar
-              </Button>
-            </VStack>
+                Esqueci minha senha
+              </Text>
+            </Link>
+            <Button
+              mt="30px"
+              type="submit"
+              w={{ base: "full", md: "300px", xl: "400px" }}
+              variant="darkPurpleWhite"
+              isLoading={loading}
+            >
+              Entrar
+            </Button>
           </VStack>
-        </HStack>
-      </Flex>
-    </Box>
+        </VStack>
+      </HStack>
+    </Flex>
   );
 }
 
-// import { Input } from "../../components/Form";
-// import { Link, useNavigate } from "react-router-dom";
-// import { gql, useMutation } from "@apollo/client";
-// import { useAuthToken } from "../../services/auth";
-// import { useState } from "react";
-
-// export const LOGIN_REQUEST = gql`
-//   mutation login($email: String!, $password: String!) {
-//     login(input: { identifier: $email, password: $password }) {
-//       jwt
-//       user {
-//         role {
-//           name
-//           type
-//         }
-//       }
-//     }
-//   }
-// `;
-
-// type User = {
-//   email: string;
-//   permissions: string[];
-//   role: string;
-// };
+// import { Link, Navigate } from "react-router-dom";
+// import { Controller, SubmitHandler, useForm } from "react-hook-form";
+// import { LOGIN_REQUEST } from "../../graphQL/mutations";
+// import { useMutation } from "@apollo/client";
+// // import { useAuth } from "../../contexts/AuthContext";
 
 // interface LoginFormValues {
 //   email: string;
@@ -190,13 +126,9 @@ export function Login({ loading }: LoginProps) {
 // }
 
 // export function Login({ loading }: LoginProps) {
-//   //   const isAuthenticated = false;
-//   const navigate = useNavigate();
-
-//   const [user, setUser] = useState();
+//   // const { isAuthenticated, login, logout } = useAuth();
 
 //   const [loginMutation] = useMutation(LOGIN_REQUEST);
-
 //   const { handleSubmit, control } = useForm<LoginFormValues>();
 
 //   const onSubmit: SubmitHandler<LoginFormValues> = async (values) => {
@@ -206,15 +138,200 @@ export function Login({ loading }: LoginProps) {
 //           email: values.email,
 //           password: values.password,
 //         },
-//         onCompleted: ({ login }) => {
-//           localStorage.setItem(AUTH_TOKEN, login.jwt);
-//           setUser(login.user.role.type);
-//           navigate("/dashboard");
-//         },
 //       });
+//       <Navigate to="/dashboard" />;
+//       console.log();
 //     } catch (error) {
 //       console.error("Erro durante o login:", error);
 //     }
 //   };
 
 //   const disableForm = loading;
+
+//   return (
+//     <Flex h="100vh" align="center">
+//       <HStack m="auto" w="85%" justify="center" align="center" gap="150px">
+//         <VStack
+//           display={{ base: "none", md: "flex" }}
+//           maxW={{ base: "400px", xl: "585px" }}
+//           textAlign="center"
+//           align="center"
+//           color="purple.900"
+//         >
+//           <Image mb="20px" src="/assets/image-login.png"></Image>
+//           <Heading mb="10px" fontSize="40px">
+//             Bem vindo ao PontoGo
+//           </Heading>
+//           <Text fontSize="25px" opacity={0.7}>
+//             Aqui você fará toda gestão do <br /> seu sistema de pontos.
+//           </Text>
+//         </VStack>
+//         <VStack gap="0px" w={{ base: "300px", xl: "400px" }} align="flex-start">
+//           <Image src="/assets/pontogo-logo-purple.svg"></Image>
+//           <Heading
+//             mt="30px"
+//             color="purple.900"
+//             fontSize={{ base: "2xl", md: "3xl", xl: "40px" }}
+//           >
+//             Faça login
+//           </Heading>
+//           <VStack
+//             as="form"
+//             onSubmit={handleSubmit(onSubmit)}
+//             w="full"
+//             gap="0px"
+//             align="flex-start"
+//           >
+//             <Controller
+//               name="email"
+//               control={control}
+//               defaultValue=""
+//               render={({ field }) => (
+//                 <Input
+//                   type="email"
+//                   label="Email"
+//                   placeholder="exemplo@email.com"
+//                   {...field}
+//                 />
+//               )}
+//             />
+//             <Controller
+//               name="password"
+//               control={control}
+//               defaultValue=""
+//               render={({ field }) => (
+//                 <Input
+//                   type="password"
+//                   label="Senha"
+//                   placeholder="***********"
+//                   icon="ant-design:eye-filled"
+//                   {...field}
+//                 />
+//               )}
+//             />
+//             <Link to="/">
+//               <Text
+//                 textDecorationLine="underline"
+//                 mt="10px"
+//                 color="purple.900"
+//                 fontSize="15px"
+//               >
+//                 Esqueci minha senha
+//               </Text>
+//             </Link>
+//             <Button
+//               mt="30px"
+//               type="submit"
+//               w={{ base: "full", md: "300px", xl: "400px" }}
+//               variant="darkPurpleWhite"
+//               isLoading={loading}
+//               disabled={disableForm}
+//             >
+//               Entrar
+//             </Button>
+//           </VStack>
+//         </VStack>
+//       </HStack>
+//     </Flex>
+//   );
+// }
+
+// import {
+//   HStack,
+//   Image,
+//   Heading,
+//   Text,
+//   VStack,
+//   Button,
+//   Flex,
+// } from "@chakra-ui/react";
+// import { Input } from "../../components/Form";
+// import { Link } from "react-router-dom";
+// import { useEffect, useState } from "react";
+
+// import { useAuth } from "../../contexts/AuthContext";
+// import { LOGIN_REQUEST } from "../../graphQL/mutations";
+// import { useMutation } from "@apollo/client";
+
+// export function Login() {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const { setUser } = useAuth();
+
+//   const [login, { data, loading }] = useMutation(LOGIN_REQUEST);
+
+//   useEffect(() => {
+//     if (!data) {
+//       return;
+//     }
+
+//     setUser(data?.login);
+//   }, [!!data]);
+
+//   return (
+//     <Flex h="100vh" align="center">
+//       <HStack m="auto" w="85%" justify="center" align="center" gap="150px">
+//         <VStack
+//           display={{ base: "none", md: "flex" }}
+//           maxW={{ base: "400px", xl: "585px" }}
+//           textAlign="center"
+//           align="center"
+//           color="purple.900"
+//         >
+//           <Image mb="20px" src="/assets/image-login.png"></Image>
+//           <Heading mb="10px" fontSize="40px">
+//             Bem vindo ao PontoGo
+//           </Heading>
+//           <Text fontSize="25px" opacity={0.7}>
+//             Aqui você fará toda gestão do <br /> seu sistema de pontos.
+//           </Text>
+//         </VStack>
+//         <VStack
+//           w={{ base: "300px", xl: "400px" }}
+//           align="flex-start"
+//           gap="30px"
+//         >
+//           <Image src="/assets/pontogo-logo-purple.svg"></Image>
+//           <VStack as="form" gap="0px" align="flex-start" w="full">
+//             <Heading
+//               color="purple.900"
+//               fontSize={{ base: "2xl", md: "3xl", xl: "40px" }}
+//               mb="23px"
+//             >
+//               Faça login
+//             </Heading>
+//             <Input
+//               type="email"
+//               name="email"
+//               label="Email"
+//               placeholder="exemplo@email.com"
+//               value={email}
+//               onChange={(event) => setEmail(event.target.value)}
+//             />
+//             <Input
+//               type="password"
+//               name="password"
+//               label="Senha"
+//               placeholder="***********"
+//               icon="ant-design:eye-filled"
+//               value={password}
+//               onChange={(event) => setPassword(event.target.value)}
+//             />
+//             <Link to="/">Esqueci minha senha</Link>
+//           </VStack>
+//           <Button
+//             type="submit"
+//             w={{ base: "full", md: "300px", xl: "400px" }}
+//             variant="darkPurpleWhite"
+//             isLoading={loading}
+//             onClick={() => {
+//               login({ variables: { email, password } });
+//             }}
+//           >
+//             Entrar
+//           </Button>
+//         </VStack>
+//       </HStack>
+//     </Flex>
+//   );
