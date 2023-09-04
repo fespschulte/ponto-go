7b5728ce8c4536ea7eee44a25802fd1b731492dd
Fernando Pinto <fschultepinto@gmail.com>
2023-09-04 16:31:14 -0300

    🚧set hooks for queries and mutations

--- src/graphQL/hooks.tsx
+++ src/graphQL/hooks.tsx
@@ -1,16 +1,14 @@
 import { useMutation } from "@apollo/client";
-import { loginMutation } from "../services/api";
+import { createRegisteredTimeMutation, loginMutation } from "./mutations";
 
 export function useLogin() {
   const [mutate, { loading }] = useMutation(loginMutation);
 
   const login = async (email: string, password: string) => {
-    const {
-      data: { dataLogin },
-    } = await mutate({
-      variables: { input: { email, password } },
+    const { data } = await mutate({
+      variables: { email, password },
     });
-    return dataLogin;
+    return data;
   };
 
   return {
@@ -19,33 +17,24 @@ export function useLogin() {
   };
 }
 
-// import { gql } from "@apollo/client";
+export function useCreateRegisteredTimeMutation() {
+  const [mutate, { loading }] = useMutation(createRegisteredTimeMutation);
 
-// import { getAccessToken } from '../auth';
+  const createRegisteredTime = async (
+    timeRegistered: string,
+    user: string,
+    published_at: string,
+    created_by: string,
+    updated_by: string
+  ) => {
+    const { data } = await mutate({
+      variables: { timeRegistered, user, published_at, created_by, updated_by },
+    });
+    return data;
+  };
 
-// const client = new GraphQLClient('http://localhost:9000/graphql', {
-//   headers: () => {
-//     const accessToken = getAccessToken();
-//     if (accessToken) {
-//       return { 'Authorization': `Bearer ${accessToken}` };
-//     }
-//     return {};
-//   },
-// });
-
-// import { gql } from "@apollo/client";
-// // import { useAuthToken } from "../services/auth";
-
-// export const LOGIN_REQUEST = gql`
-//   mutation login($email: String!, $password: String!) {
-//     login(input: { identifier: $email, password: $password }) {
-//       jwt
-//       user {
-//         role {
-//           name
-//           type
-//         }
-//       }
-//     }
-//   }
-// `;
+  return {
+    createRegisteredTime,
+    loading,
+  };
+}
