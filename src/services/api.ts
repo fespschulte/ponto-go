61781afc47d721bde41ea26bb8f61a1c6640cb05
Fernando Pinto <fschultepinto@gmail.com>
2023-09-04 16:35:03 -0300

    🚧set apollo client configuration

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,6 @@ import {
   ApolloLink,
   concat,
   createHttpLink,
-  gql,
   InMemoryCache,
 } from "@apollo/client";
 import { getAccessToken } from "./auth";
@@ -26,34 +25,3 @@ export const client = new ApolloClient({
   link: concat(authLink, httpLink),
   cache: new InMemoryCache(),
 });
-
-export const loginMutation = gql`
-  mutation login($email: String!, $password: String!) {
-    login(input: { identifier: $email, password: $password }) {
-      jwt
-    }
-  }
-`;
-
-// import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
-// import { setContext } from "@apollo/client/link/context";
-// import { useAuthToken } from "./auth";
-
-// const httpLink = createHttpLink({
-//   uri: "http://test.frontend.api.brainny.cc/graphql",
-// });
-
-// const authLink = setContext((_, { headers }) => {
-//   const token = useAuthToken();
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : "",
-//     },
-//   };
-// });
-
-// export const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
